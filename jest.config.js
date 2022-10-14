8ebce8ae25347f248f4a80586c97adf44d7cdd02
Kevin Beltrao de Melo <kbmelo@outlook.com>
2022-10-14 18:45:16 -0300

    feat(handlers): :sparkles: adding middlewares to handle errors and responses from api

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+};
---
+++ tests/app.e2e.test.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest';
+import app from '../src/config/app';
+
+const request = supertest(app);
+
+describe('Testing app.ts', () => {
+  test('It should responde the Get method', async () => {
+    const response = await request.get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBe('Hello, world');
+  });
+});
